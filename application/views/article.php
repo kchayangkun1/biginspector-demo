5fc2bee389e7337b6a9ecbc45506700243867ea3
kchayangkun1 <khunchayangkun.t@gmail.com>
2021-11-23 17:54:53 +0700

    font end v.2
    update
    - แกลลอรี่  Gallery
    - คอนเทนต์ Contact

--- application/views/article.php
+++ application/views/article.php
@@ -15,8 +15,9 @@
                 <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mt-5 pt-5">
                     <h3 id="gallery-product" style="margin-top: 0px; text-align: center;"><b>โครงการ Piano Comodo</b></h3>
                     <h4 id="gallery-product" style="margin-top: 0px; text-align: center;">หากคุณกำลังตัดสินใจตกแต่งหรือรีโนเวทบ้านสักหลัง แต่กลัวว่านานไปแล้วจะเบื่อ วันนี้ 32 Interior Design มีสไตล์การออกแบบที่อยู่อย่างไรก็ไม่เบื่อ นานแค่ไหนก็ดูไม่เก่ามาให้ชมกัน(ครับ/ค่ะ)</h4>
-                    
-                    <button type="button" class="btn btn-outline-light" style="border-radius: 12px;">View All</button>
+                    <div class="button" style="text-align: center;">
+                        <button type="button" class="btn btn-outline-light" style="border-radius: 12px;">View All</button>
+                    </div>
                 </div>
                 <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
                     <img src="<?=base_url('assets/img/detail/gallery-piano.png');?>" class="img-rounded" id="accounting2">
@@ -25,3 +26,12 @@
     </div><!-- end container -->
 </section>
 
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4"></div>
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4"></div>
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4"></div>
+        </div>
+    </div>
+</section>
